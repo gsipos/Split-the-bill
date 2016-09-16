7c4b37af92610580e23721f6fb732df5946c2266
Gergely Sipos <gergely.sipos@doctusoft.com>
2016-09-16 22:27:27 +0200

    console debug to log

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import * as startup from  './startup/startup';
 import * as ApplicationRole from './application-role';
 import WebRole from './web/web-role';
 
-console.debug('STB starting...');
+console.log('STB starting...');
 startup.setUpTables();
 
 var roleInitializer = new ApplicationRole.Initializer();
@@ -10,4 +10,4 @@ roleInitializer.registerRole(new WebRole());
 
 roleInitializer.startRoles();
 
-console.debug('STB end :(');
+console.log('STB end :(');
